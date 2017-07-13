99948dbf04d1fb31d41d03e5cc87ede1a7d79193
lyaohe <lyaohe@gmail.com>
2017-07-14 02:44:44 +0800

    pds 1.0

--- README.md
+++ README.md
@@ -1,6 +1,35 @@
-# PDS(proxy download server)
-PDS -- proxy download server(代理下载服务、透传下载服务)
+# PDS (Proxy Download Server)
+PDS -- Proxy Download Server(代理下载服务器)
+- 基于PHP Curl实现代理下载服务器，也可以当作 代理请求服务器、中转服务器
+- 分片输出，降低PHP内存压力，可做 PHP大文件下载
+- CURLOPT_WRITEFUNCTION 参数的 example 代码
 
 Request:
 
-> index.php?url=http://example.com/
+> pds.php?url=http://example.com/
+
+Debug:
+
+> pds.php?url=http://example.com/&debug=1
+
+### 项目灵感
+有一次在Github下载软件安装包，国内网络下载超慢，想到了代理下载，或者用中转服务器来下载快很多，就开始设计和调研，并动手写个代理下载服务。
+
+你们还想什么场景用到，欢迎跟我交流
+
+Email:  lyaohe@gmail.com
+
+### 正在解决的问题
+1. 请求百度有点问题
+- `pds.php?url=https://www.baidu.com`  请求失败
+- `pds.php?url=https://www.baidu.com&debug=1`  Debug输出是成功，还在分析header报文
+
+2. 多考虑一些异常情况
+
+
+### 计划继续开发的功能
+1. 支持post请求、支持cookie透传，还在想哪些场景需要
+2. 简单的网页代理，返回的Body内容的链接替换成代理链接
+3. 复杂的网页代理，JS的异步请求链接也替换成代理链接
+
+更新时间: 2017.07.14
--- pds.php
+++ pds.php
@@ -6,37 +6,97 @@
  * Date: 2017/7/12
  * Time: 上午12:11
  */
-class pds
+
+/**
+ * Pds(Proxy Download Server) 代理下载服务器
+ */
+class Pds
 {
-    static public function request($url, $user_agent=''){
-        $ch = curl_init();
-        curl_setopt($ch,CURLOPT_URL, $url);
-        if (strtoupper($_SERVER['REQUEST_METHOD']) == 'POST' ) {
-            curl_setopt( $ch, CURLOPT_POST, true );
-            curl_setopt( $ch, CURLOPT_POSTFIELDS, $_POST );
-        }
+    static public $debug = false;
 
-        $cookie = array();
-        foreach ( $_COOKIE as $key => $value ) {
-            $cookie[] = $key . '=' . $value;
-        }
-        $cookie = implode( '; ', $cookie );
+    static public function request($url, $debug = false){
+        self::$debug = $debug;
 
-        curl_setopt( $ch, CURLOPT_COOKIE, $cookie );
+        $ch = curl_init($url);
 
-        curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, true );
-        curl_setopt( $ch, CURLOPT_HEADER, true );
-        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);//将curl_exec()获取的信息以文件流的形式返回，而不是直接输出
+        curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);//在启用CURLOPT_RETURNTRANSFER的时候，返回原生的（Raw）输出
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);//启用时会将服务器服务器返回的"Location: "放在header中递归的返回给服务器
+        curl_setopt( $ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']); // HTTP_USER_AGENT
+        curl_setopt($ch, CURLOPT_HEADER, true); //输出header
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 60);//在发起连接前等待的时间，如果设置为0，则无限等待
+        curl_setopt($ch, CURLOPT_TIMEOUT, 60*60*2);//设置cURL允许执行的最长秒数
+        curl_setopt($ch, CURLOPT_BUFFERSIZE, 20971520);//每次获取的数据中读入缓存的大小，但是不保证这个值每次都会被填满
 
-        curl_setopt( $ch, CURLOPT_USERAGENT, $user_agent != '' ? $user_agent : $_SERVER['HTTP_USER_AGENT'] );
+        $flag=0;
+        //回调函数名。该函数应接受两个参数。第一个是 cURL resource；第二个是要写入的数据字符串
+        curl_setopt($ch, CURLOPT_WRITEFUNCTION, function($ch ,$str) use (&$flag){
+            //$str 可以简单理解成每次回调，取一行数据,但超过缓存区设置大小也会被截取
+            $len = strlen($str);
 
-        list( $header, $contents ) = preg_split( '/([\r\n][\r\n])\\1/', curl_exec( $ch ), 2 );
-        //$contents = str_replace(array("\n", "\r"), '', $contents);
-        $status = curl_getinfo( $ch );
+            //echo "len:{$len};str:{$str}";
 
-        curl_close( $ch );
+            switch ($flag)
+            {
+                case 0:
 
-        return array($header, $contents, $status);
+                    if(strpos($str, 'HTTP/1.1 200') !== false ||
+                        strpos($str, 'HTTP/1.1 404') !== false || strpos($str, 'HTTP/1.1 500') !== false){
+                        // todo 这个判断条件可以优化
+                        //当标志位flag == 0, 且找到 (HTTP/1.1 200 || HTTP/1.1 404 || HTTP/1.1 500)，标志位flag改为1
+                        self::pdsHeader($str);
+                        $flag = 1;
+                    }
+
+                    break;
+                case 1:
+                    if($len > 2){  //当标志位flag == 1, 且 len > 2,是 header 字符串
+                        self::pdsHeader($str);
+                    }else{
+                        // header与body之间有两个回车换行，所以有一行是"\r\n"，2个字符
+                        // 表示 “\r\n” 必须要用双引号
+                        if($str === "\r\n"){
+                            $flag = 2; //标志位flag改为2，后面是正文内容
+                        }
+                    }
+                    break;
+                case 2:
+                    self::pdsBody($str, $len);
+                    break;
+            }
+
+            return $len;
+        });
+
+        curl_exec($ch);
+        curl_close($ch);
     }
 
+    private static function pdsHeader($headerStr){
+        if(self::$debug){
+            echo "header: {$headerStr}<br>";
+        }else{
+            header($headerStr);
+        }
+    }
+
+    private static function pdsBody($bodyStr, $len){
+        if(self::$debug){
+            echo "body: {$len}<br>";
+        }else{
+            echo $bodyStr; // 输出内容 body
+        }
+    }
 }
+
+
+
+if(empty($_GET['url'])){
+    echo "Request: pds.php?url=http://example.com <br>";
+    echo "请求链接: pds.php?url=http://example.com <br>";
+    exit();
+}
+$debug = empty($_GET['debug']) ? false : true;
+$url = $_GET['url'];
+
+Pds::request($url, $debug);
